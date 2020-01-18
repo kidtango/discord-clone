32bb84857200b6e432fa609ad79804ca866268da
kidtango <scott.q.tang@gmail.com>
2020-01-17 21:21:05 -0600

    Updates

---
+++ precache-manifest.ea137e548693ff92a3a108c977d22425.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c59189e7f7c6e9173cad5b126f9961",
+    "url": "kidtango.github.io/discord-clone/index.html"
+  },
+  {
+    "revision": "c0326a4408e86e9db3f4",
+    "url": "kidtango.github.io/discord-clone/static/css/main.a76997b5.chunk.css"
+  },
+  {
+    "revision": "68fd2984b0c7d874c82b",
+    "url": "kidtango.github.io/discord-clone/static/js/2.c77d9af6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "kidtango.github.io/discord-clone/static/js/2.c77d9af6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0326a4408e86e9db3f4",
+    "url": "kidtango.github.io/discord-clone/static/js/main.b9020ea7.chunk.js"
+  },
+  {
+    "revision": "6b40b9e0100e19c92893",
+    "url": "kidtango.github.io/discord-clone/static/js/runtime-main.31b7d3e1.js"
+  },
+  {
+    "revision": "8a1174fdc6aa6c61e4d4abf211f09a5e",
+    "url": "kidtango.github.io/discord-clone/static/media/avatar2.8a1174fd.jpg"
+  },
+  {
+    "revision": "a2e6f5628ffb30cc9c8acb829293a3f0",
+    "url": "kidtango.github.io/discord-clone/static/media/icon_discord.a2e6f562.svg"
+  },
+  {
+    "revision": "07df5d5b2c7148f64077369db40349ef",
+    "url": "kidtango.github.io/discord-clone/static/media/icon_laravel.07df5d5b.svg"
+  },
+  {
+    "revision": "3d86bb4f68b916ebb9acccb1e12c142e",
+    "url": "kidtango.github.io/discord-clone/static/media/icon_tailwind.3d86bb4f.svg"
+  }
+]);
